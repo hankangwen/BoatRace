e8dd40fb2514d5b845863a02c502c57302062436
kangwen <1397796710@qq.com>
2022-08-15 12:44:29 +0800

    Create Boat Client.

---
+++ Client/Assets/Scripts/GameMain.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Boat Race Client Start.");
+    }
+}
