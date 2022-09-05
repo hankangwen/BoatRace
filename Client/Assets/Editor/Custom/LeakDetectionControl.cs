fc20f2a9db7313ad9d35d758093f1db0c6052165
kangwen <1397796710@qq.com>
2022-09-05 20:09:11 +0800

    NativeLeakDetection

---
+++ Client/Assets/Editor/Custom/LeakDetectionControl.cs
@@ -0,0 +1,27 @@
+using Unity.Collections;
+using UnityEditor;
+
+namespace BoatRace.Editor
+{
+    public static class LeakDetectionControl
+    {
+        [MenuItem("Tools/Jobs/Leak Detection")]
+        private static void LeakDetection()
+        {
+            NativeLeakDetection.Mode = NativeLeakDetectionMode.Enabled;
+        }
+
+        [MenuItem("Tools/Jobs/Leak Detection With Stack Trace")]
+        private static void LeakDetectionWithStackTrace()
+        {
+            NativeLeakDetection.Mode = NativeLeakDetectionMode.EnabledWithStackTrace;
+        }
+        
+        
+        [MenuItem("Tools/Jobs/No Leak Detection")]
+        private static void NoLeakDetection()
+        {
+            NativeLeakDetection.Mode = NativeLeakDetectionMode.Disabled;
+        }
+    }
+}
