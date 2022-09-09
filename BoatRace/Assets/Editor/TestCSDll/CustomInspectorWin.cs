dedd4c728996ca62c82413aa6156f92184629e48
kangwen <1397796710@qq.com>
2022-09-09 11:35:23 +0800

    测试反射

---
+++ BoatRace/Assets/Editor/TestCSDll/CustomInspectorWin.cs
@@ -0,0 +1,34 @@
+
+
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class CustomInspectorWin
+{
+    [MenuItem("打包AssetBundle/Lock&unlock")]
+
+    static void OnLock()
+    {
+        
+
+        // var type = typeof(EditorWindow).Assembly.GetType("UnityEditor.InspectorWindow");
+        //
+        // var window = EditorWindow.GetWindow(type);
+        //
+        // // MethodInfo info = type.GetMethod("FlipLocked", BindingFlags.NonPublic | BindingFlags.Instance);
+        //
+        // // info.Invoke(window, null);
+        //
+        // PropertyInfo title = type.GetProperty("title");
+        // title.SetValue(window, "AABBCC", null);
+        //
+        // MethodInfo isLock = type.GetMethod("RefreshTitle", BindingFlags.NonPublic | BindingFlags.Instance);
+        // isLock.Invoke(window, null);
+        //
+        // PropertyInfo t2 = type.GetProperty("titleContent");
+        // t2.SetValue(window, new GUIContent("宝宝巴士", "This is the 宝宝巴士"), null);
+        
+    }
+    
+}
